837742f8aa15afdbbc9dbc1e55451576071c4546
chrismdating1206@gmail.com <christian.dating12>
2023-04-16 22:54:01 +0800

    Pseudocode

--- decrypt.py
+++ decrypt.py
@@ -8,3 +8,13 @@ greet = "GOOD DAY!"
 print("\033[92m" + pyfiglet.figlet_format(greet, font = "bubble"))
 
 print("\033[90m=" *80)
+
+#Pseudocode
+#ask user for input
+#check each characters
+#if '*' , change to a
+#if '&' , change to e
+#if '#' , change to i
+#if '+' , change to o
+#if '!' , change to u
+#print output
