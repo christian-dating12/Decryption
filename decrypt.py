b204a1daa0681f293f986207221fa4f636ef4f55
chrismdating1206@gmail.com <christian.dating12>
2023-04-16 23:17:50 +0800

    '+' is working

--- decrypt.py
+++ decrypt.py
@@ -24,9 +24,11 @@ for i in range(len(input_str)):
 #if '#' , change to i
     elif input_str[i] == "#":
         output_str += "i"
+#if '+' , change to o
+    elif input_str[i] == "+":
+        output_str += "o"
     else :
         output_str += input_str[i]
 
-#if '+' , change to o
 #if '!' , change to u
 #print output
